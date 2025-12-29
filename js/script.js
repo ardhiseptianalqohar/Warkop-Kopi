e3d44be96e1361d52be80606ec91b43d0571c49d
alqohar <ardhiseptianalqohar@gmail.com>
2025-12-29 21:19:15 +0700

    Update all

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// toggle class avtive
+// toggle class avtive untuk hamburegr menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
@@ -6,12 +6,27 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan nav nya
+// toggle class avtive untuk search-form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik di luar element
 
 const hamburger = document.querySelector("#hamburger-menu");
+const sb = document.querySelector('#search-button');
+
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
